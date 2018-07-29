c967957b8970d7bf88acdeca9f1dbb43327d6a5a
jekatibor <jeka.tibor@gmail.com>
2018-07-29 22:59:33 +0300

    tickets table

--- packages/kaban-web-app/components/TicketsTable.vue
+++ packages/kaban-web-app/components/TicketsTable.vue
@@ -1,9 +1,28 @@
 <template>
-	<div>
-		<div v-for="ticket in tickets" :key="ticket.key">
-			{{ ticket.name }}
+	<b-container fluid>
+		<div class='tickets-table'>
+			<div v-for="ticket in tickets" :key="ticket.key">
+				<div class="ticket-line">
+					<div class="ticket-line-type">
+						<img
+							:src="ticket.type.iconUrl"
+							:title="ticket.type.name">
+					</div>
+					<div class="ticket-line-key"></div>
+					<div class="ticket-line-title"><span>{{ ticket.name }}</span></div>
+					<div class="ticket-line-name">Name</div>
+					<div class="ticket-line-name">Name</div>
+					<div>Block</div>
+					<div class="ticket-line-status">Status</div>
+					<div class="ticket-line-resolution">Unresolved</div>
+					<div class="ticket-line-date">19/07/2018</div>
+					<div class="ticket-line-date">19/07/2018</div>
+					<div class="ticket-line-date">19/07/2018</div>
+					<div class="ticket-line-action"><a href="#"><font-awesome-icon icon="ellipsis-h" /></a></div>
+				</div>
+			</div>
 		</div>
-	</div>
+	</b-container>
 </template>
 
 <script>
