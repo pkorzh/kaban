e223dc9898eadaa6b80b90d1081da76a40e6bce9
Platon Korzh <platonkorzh@Platons-MacBook-Pro.local>
2018-08-25 12:31:32 +0300

    show backlogs in boards view

--- packages/kaban-web-app/pages/boards/index.vue
+++ packages/kaban-web-app/pages/boards/index.vue
@@ -19,35 +19,58 @@
 			<div class="board-table-holder">
 				<div class="table">
 					<div class="table-line table-line-head">
-						<div class="table-line-name">Name</div>
+						<div>Name</div>
+						<div class="table-line-name">Backlog</div>
 						<div class="text-center">
 							Lead time
 						</div>
 						<div class="text-center">
 							Circle time
 						</div>
+						<div class="text-center">
+							P99
+						</div>
 						<div class="table-line-action"></div>
 					</div>
 
-					<div class="table-line" v-for="board in boardList" :key="board.key">
-						<div class="table-line-name">
-							<nuxt-link :to="{name: 'boards-key', params: {key: board.key}}">
-								{{ board.name }}
-							</nuxt-link>
+					<div v-for="board in boardList" :key="board.key">
+						<div class="table-line">
+							<div>
+								<nuxt-link :to="{name: 'boards-key', params: {key: board.key}}">
+									{{ board.name }}
+								</nuxt-link>
+							</div>
+							<div class="table-line-name"></div>
+							<div class="text-center"></div>
+							<div class="text-center"></div>
+							<div class="text-center"></div>
+							<div class="table-line-action">
+								<b-dropdown variant="link" no-caret>
+									<template slot="button-content">
+										<font-awesome-icon icon="ellipsis-v" />
+									</template>
+									<b-dropdown-item href="">Start</b-dropdown-item>
+								</b-dropdown>
+							</div>
 						</div>
-						<div class="text-center">
-							<Sparkline />
-						</div>
-						<div class="text-center">
-							<Sparkline />
-						</div>
-						<div class="table-line-action">
-							<b-dropdown variant="link" no-caret>
-								<template slot="button-content">
-									<font-awesome-icon icon="ellipsis-v" />
-								</template>
-								<b-dropdown-item href="">Start</b-dropdown-item>
-							</b-dropdown>
+
+						<div class="table-line" v-for="backlog in board.backlogs" :key="backlog.key">
+							<div></div>
+							<div class="table-line-name">
+								<nuxt-link :to="{name: 'backlogs-key', params: {key: backlog.key}}">
+									{{ backlog.name }}
+								</nuxt-link>
+							</div>
+							<div class="text-center">
+								<Sparkline />
+							</div>
+							<div class="text-center">
+								<Sparkline />
+							</div>
+							<div class="text-center">
+								10 days
+							</div>
+							<div class="table-line-action"></div>
 						</div>
 					</div>
 				</div>
