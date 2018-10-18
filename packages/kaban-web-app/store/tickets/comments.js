24e47d2ac175c0e19d679e235bc89e2d5115a844
platon korzh <platon@korzh.io>
2018-10-18 17:22:17 +0300

    dont store comments inside store

--- packages/kaban-web-app/store/tickets/comments.js
+++ packages/kaban-web-app/store/tickets/comments.js
@@ -1,3 +1,15 @@
 import moduleFactory from '../module-factory'
 
-export default moduleFactory('comments', {})
+export default moduleFactory('comments', {
+	mutations: {
+		REMOVE(state, payload) {
+			// pass
+		},
+		STAGE(state, payload) {
+			// pass
+		},
+		STAGE_MULTIPLE(state, multiple) {
+			// pass
+		},
+	}
+})
