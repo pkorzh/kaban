8761b052223e60d793010ed04e869701db31110c
platon korzh <platon@korzh.io>
2018-11-10 14:40:55 +0200

    Kaban Configuration

--- packages/web-app/pages/kaban-configuration/notification/index.vue
+++ packages/web-app/pages/kaban-configuration/notification/index.vue
@@ -2,13 +2,19 @@
 	<b-container fluid>
 		<b-card-group deck>
 			<b-card title="Telegram">
+				<span 
+					class="badge badge-success" 
+					v-if="notificationStatus === 'telegram'"
+					v-t="'active'">
+				</span>
+
 				<p class="card-text">
 Telegram is a cloud-based mobile and desktop messaging app with a focus on security and speed.
 				</p>
 				<nuxt-link 
 					:to="localePath({name: 'kaban-configuration-notification-telegram'})"
 					class="btn btn-outline-success">
-					<span v-t="'setup'"></span>
+					{{ actionButtonText('telegram') }}
 				</nuxt-link>
 			</b-card>
 		</b-card-group>
@@ -23,10 +29,20 @@ Telegram is a cloud-based mobile and desktop messaging app with a focus on secur
 			return {
 				title: this.$t('notificationConfig')
 			}
+		},
+		computed: {
+			...mapGetters('kabanConfiguration', [
+				'notificationStatus',
+			]),
+		},
+		methods: {
+			actionButtonText(type) {
+				if (this.notificationStatus === type) {
+					return this.$t('change');
+				} else {
+					return this.$t('setup');
+				}
+			}
 		}
 	}
 </script>
-
-<style>
-
-</style>
--- packages/web-app/pages/kaban-configuration/storage/index.vue
+++ packages/web-app/pages/kaban-configuration/storage/index.vue
@@ -2,13 +2,20 @@
 	<b-container fluid>
 		<b-card-group deck>
 			<b-card title="S3">
+				<span 
+					class="badge badge-success" 
+					v-if="storageStatus === 's3'"
+					v-t="'active'">
+				</span>
+
 				<p class="card-text">
 					Use AWS Simple Storage Service
 				</p>
+
 				<nuxt-link 
 					:to="localePath({name: 'kaban-configuration-storage-s3'})"
 					class="btn btn-outline-success">
-					<span v-t="'setup'"></span>
+					{{ actionButtonText('s3') }}
 				</nuxt-link>
 			</b-card>
 		</b-card-group>
@@ -22,6 +29,20 @@
 		head() {
 			return {
 				title: this.$t('storageConfig')
+			}
+		},
+		computed: {
+			...mapGetters('kabanConfiguration', [
+				'storageStatus',
+			]),
+		},
+		methods: {
+			actionButtonText(type) {
+				if (this.storageStatus === type) {
+					return this.$t('change');
+				} else {
+					return this.$t('setup');
+				}
 			}
 		}
 	}
